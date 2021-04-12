2ce53d490d76fa27f766de135a0bfb305ded4e07
Jagathratchakan <82392148+Jagathratchakan@users.noreply.github.com>
2021-04-12 20:19:26 +0530

    Update and rename Hello.c to Swap two number without using temporary variable.c
    To swap two number without using third variable

---
+++ Swap two number without using temporary variable.c
@@ -0,0 +1,12 @@
+#include<studio.h>
+int main()
+{
+ int a,b;
+ scanf("%d%d",&a,&b);
+ printf("Before swap a=%d and b=%d",a,b);
+ a=a+b;
+ b=a-b;
+ a=a-b;
+ printf("After swap a=%d and b=%d",a,b);
+ return 0;
+}
